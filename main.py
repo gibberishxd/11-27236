073d42161c709641c48ea440525443e4f4fe5958
gibberishxd <gogiberidzetornike8@gmail.com>
2024-03-24 14:36:09 +0100

    task 6

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-squares = [x ** 2 for x in range(1, 11)]
-print("List of squares from 1 to 10:", squares)
+from square_generator import SquareGenerator
 
-
-def range_of_nums_squares(s, e):
-    return [x ** 2 for x in range(s, e + 1)]
-
-
-start = 1
-end = 10
-squares_list = range_of_nums_squares(start, end)
-print(f"List of squares from {start} to {end}: {squares_list}")
-
-
-class SquareGenerator:
-    def generate_squares(self, start, end):
-        if end < start:
-            raise ValueError("End of the range cannot be less than the start.")
-        return [x ** 2 for x in range(start, end + 1)]
-
-
-
+# Example usage:
 square_gen = SquareGenerator()
 start = 1
 end = 10
---
+++ square_generator.py
@@ -0,0 +1,5 @@
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("End of the range cannot be less than the start.")
+        return [x ** 2 for x in range(start, end + 1)]
