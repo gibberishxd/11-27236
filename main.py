11ba930a79e501160311b7166f5eb507e87b6df2
gibberishxd <gogiberidzetornike8@gmail.com>
2024-03-24 14:33:38 +0100

    task 3

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ end = 10
 squares_list = range_of_nums_squares(start, end)
 print(f"List of squares from {start} to {end}: {squares_list}")
 
+
+
+class SquareGenerator:
+
+    def generate_squares(self, start, end):
+        return [x ** 2 for x in range(start, end + 1)]
+
+
+square_gen = SquareGenerator()
+start = 1
+end = 10
+squares_list = square_gen.generate_squares(start, end)
+print(f"List of squares from {start} to {end}: {squares_list}")
