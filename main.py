eb2feaba1b55bc8a9ae1090c0ac7abecfa4107d8
gibberishxd <gogiberidzetornike8@gmail.com>
2024-03-24 14:30:36 +0100

    task 2

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ squares = [x ** 2 for x in range(1, 11)]
 print("List of squares from 1 to 10:", squares)
 
 
+def range_of_nums_squares(s, e):
+    return [x ** 2 for x in range(s, e + 1)]
+
+
+start = 1
+end = 10
+squares_list = range_of_nums_squares(start, end)
+print(f"List of squares from {start} to {end}: {squares_list}")
 
